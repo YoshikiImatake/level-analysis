e5f25d10db1687a56fa77f33ba4f55305704aeb8
Yoshiki Imatake <y.imatake@gmail.com>
2018-11-13 11:57:34 +0900

    文単位で区切る student01.txtはサンプルテキスト

--- Analyzer.py
+++ Analyzer.py
@@ -1,7 +1,13 @@
+import re
+
 class Analyzer:
     def __init__(self, text, reviewed):
-        print(text)
-   # def sentence_length:
+        self.text = text
+        self.reviewed = reviewed
+ 
+    def sentence_length(self, text):
+        text_splitted = re.split('\n|。', text)
+        print(text_splitted[0])
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import Analyzer
 
+
 full_text = ""
 while True:
 	try:
 		text = input()
+		text += "\n"
 		full_text += text
 	except EOFError:
 		break
 
 a1 = Analyzer.Analyzer(full_text, False)
+a1.sentence_length(full_text)
