80bbc25faf9f7c183e7f17401bfedc74b02ff507
y_imatake <y.imatake@gmail.com>
2018-11-29 13:59:25 +0900

    MeCabによって形態素解析されたテキストファイルを読み込んで、各品詞の出現回数をカウントする

---
+++ mcb_main.py
@@ -0,0 +1,19 @@
+import Analyzer
+
+pos_freq = {} #各品詞の出現数を記録する辞書
+while True:
+    try:
+        text = input()
+        if text == "EOS":
+            pass #EOSの行をsplitするとa[1]がout of rangeになる
+        else:
+            a = text.split('\t')
+            b = a[1].split(',')
+            pos_freq[b[0]] = pos_freq.get(b[0], 0) + 1
+                #該当する品詞の出現数（デフォルト値０）に１を足す。
+    except EOFError:
+        break
+
+for pos in pos_freq:
+    freq = pos_freq[pos]
+    print(f"{pos}:{freq}")
